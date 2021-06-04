f50dca7d1fae1173f6b64831161c162064400bba
Сергей <2006lsd2006@gmail.com>
2021-06-04 09:57:21 +0300

    Yeah?

--- web/pages/gameplay.js
+++ web/pages/gameplay.js
@@ -5,15 +5,12 @@ window.onresize = function (){
 };
 
 const body = document.querySelector("body");
-const sun = document.querySelectorClass("sun_main");
-const moon = document.querySelectorClass("moon_main");
-const airdrop = document.querySelectorClass("airdrop");
+const sun = document.querySelector(".sun_main");
+const moon = document.querySelector(".moon_main");
+const airdrop = document.querySelector(".airdrop");
 const bonus = document.querySelector(".bonus");
-const life = document.querySelector(".lifeK");
-const meal = document.querySelector(".mealK");
-const points = document.querySelector(".pointsK")
 
-let cycles = 0;
+let cycles = 1;
 let level = 1;
 let lifePoints = 10;
 let mealPoints = 10;
@@ -37,10 +34,12 @@ if (lifePoints <= 0 || mealPoints <= 0 || pointsPoints <= 0){
 }
 
 if (cycles % 12 == 0){
+    airdrop.classList.add("falldrop")
     lifePoints += 5;
     mealRemove += 5;
     pointsPoints += 5;
 } else if (cycles % 7 == 0) {
+    bonus.classList.add("B_present")
     lifePoints += 3;
     mealRemove += 3;
     pointsPoints += 3;
@@ -48,11 +47,15 @@ if (cycles % 12 == 0){
 
 if (cycles % 15 == 0){
     if (body.classList.contains("dayBody")){
-        body.classList.remove("dayBody")
-        body.classList.add("nightBody")
+        body.classList.remove("dayBody");
+        body.classList.add("nightBody");
+        sun.classList.remove("day");
+        moon.classList.add("night");
     } else {
         body.classList.remove("nightBody")
         body.classList.add("dayBody")
+        sun.classList.add("day");
+        moon.classList.remove("night");
     }
 }
 
@@ -64,4 +67,34 @@ if (cycles % 17 == 0 && level < 3) {
     mealRemove += 1;
     pointsAdd += 1.5;
     pointsRemove += 1;
+}
+
+function lifeAddFunction() {
+    cycles += 1;
+    lifePoints += lifeAdd;
+    pointsPoints -= pointsRemove;
+    mealPoints -= mealRemove;
+    document.getElementById("lifeK").textContent = lifePoints;
+    document.getElementById("mealK").textContent = mealPoints;
+    document.getElementById("pointsK").textContent = pointsPoints;
+}
+
+function mealAddFunction() {
+    cycles += 1;
+    lifePoints -= lifeRemove;
+    pointsPoints -= pointsRemove;
+    mealPoints += mealAdd;
+    document.getElementById("lifeK").textContent = lifePoints;
+    document.getElementById("mealK").textContent = mealPoints;
+    document.getElementById("pointsK").textContent = pointsPoints;
+}
+
+function pointsAddFunction() {
+    cycles += 1;
+    lifePoints -= lifeRemove;
+    pointsPoints += pointsAdd;
+    mealPoints -= mealRemove;
+    document.getElementById("lifeK").textContent = lifePoints;
+    document.getElementById("mealK").textContent = mealPoints;
+    document.getElementById("pointsK").textContent = pointsPoints;
 }
